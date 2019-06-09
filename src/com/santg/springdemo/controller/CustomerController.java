6cc3b5c88ce9018e244e66def0cd44d5a3697cdd
Santiago Grangetto <santiagograngetto@gmail.com>
2019-06-08 21:27:38 -0300

    Implement CustomerController

--- src/com/santg/springdemo/controller/CustomerController.java
+++ src/com/santg/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.santg.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.santg.springdemo.dao.CustomerDAO;
+import com.santg.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// inject the customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		
+		// get customers from the dao
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute("customers", customers);
 		
 		return "list-customers";
 	}
