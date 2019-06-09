f6bd7969961b379622393f0d3092598826e7d280
Santiago Grangetto <santiagograngetto@gmail.com>
2019-06-08 22:42:00 -0300

    implement customer service
    implement a service between the CustomerController and the CustomerDAO

--- src/com/santg/springdemo/controller/CustomerController.java
+++ src/com/santg/springdemo/controller/CustomerController.java
@@ -5,24 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.santg.springdemo.dao.CustomerDAO;
 import com.santg.springdemo.entity.Customer;
+import com.santg.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// inject the customer DAO
+	// inject customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
-		// get customers from the dao
-		List<Customer> customers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> customers = customerService.getCustomers();
 		
 		// add the customers to the model
 		model.addAttribute("customers", customers);
---
+++ src/com/santg/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.santg.springdemo.service;
+
+import java.util.List;
+
+import com.santg.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
