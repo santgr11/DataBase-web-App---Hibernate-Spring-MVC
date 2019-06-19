f65f002b01b822d18097f462819171ec21480f49
Santiago Grangetto <santiagograngetto@gmail.com>
2019-06-19 14:41:44 -0300

    Add @Before advice + fix pointcut typo

--- src/com/santg/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/santg/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.santg.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,20 +16,28 @@ public class CRMLoggingAspect {
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
-	@Pointcut("execution(* com.santg.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.santg.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.santg.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.santg.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.santg.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.santg.springdemo.dao.*.*(..))")
 	private void forDAOPackage() {}
 
 	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
 	private void forAppFlow() {}
 	
 	// add @Before advice
-	
+	@Before("forAppFlow()")
+	public void before(JoinPoint joinPoint) {
+		
+		// display method we are calling
+		logger.info("===> in @Before: calling method: " + joinPoint.getSignature().toShortString());
+		
+		// display the arguments to the method
+		
+	}
 	
 	// add @AfterReturning advice
 }
